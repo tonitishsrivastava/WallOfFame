be8f2a52aabd54bb4f7e770b53ec163a15934f19
Nitish Srivastava <nitishsrivastava@Nitishs-MacBook-Air.local>
2019-01-22 22:09:39 +0530

    Facebook Login done

---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '9.0'
+
+target 'WallOfFame' do
+
+  use_frameworks!
+
+
+    pod 'FBSDKLoginKit'
+    pod 'FacebookLogin'
+    
+
+end
--- WallOfFame/AppDelegate.swift
+++ WallOfFame/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FacebookCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        SDKApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
+        // Add any custom logic here.
         return true
     }
 
@@ -40,6 +44,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        let appId: String = SDKSettings.appId
+        if url.scheme != nil && url.scheme!.hasPrefix("fb\(appId)") && url.host ==  "authorize" {
+            return SDKApplicationDelegate.shared.application(app, open: url, options: options)
+        }
+        return false
+    }
+
 
 
 }
